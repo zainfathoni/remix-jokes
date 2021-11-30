a3ad5221722b8924fc086adece9efcf369e0e4eb
Zain Fathoni <me@zainf.dev>
2021-11-30 21:52:28 +0800

    Mutations: a simple mutation
    https://remix.run/docs/en/v1/tutorials/jokes#mutations

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,23 @@
+import type { ActionFunction } from 'remix'
+import { redirect } from 'remix'
+import { db } from '~/utils/db.server'
+
+export let action: ActionFunction = async ({ request }) => {
+  let form = await request.formData()
+  let name = form.get('name')
+  let content = form.get('content')
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (typeof name !== 'string' || typeof content !== 'string') {
+    throw new Error(`Form not submitted correctly.`)
+  }
+
+  let fields = { name, content }
+
+  let joke = await db.joke.create({ data: fields })
+  return redirect(`/jokes/${joke.id}`)
+}
+
 export default function NewJokeRoute() {
   return (
     <div>
